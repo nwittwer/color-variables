b5a45fbed95cc1fcedbe37b10a31d32dc79ea3d5
Nick Wittwer <nwittwer@clincase.com>
2020-08-13 22:21:48 +0200

    Added copy all styles to clipboard feature

--- components/sidebar/overrides/ColorVariable.vue
+++ components/sidebar/overrides/ColorVariable.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
   div
     button(@click="removeStyles") Remove overrides
+    button(@click="copyToClipboard") Copy overrides
     div(v-for="style of styles")
       h4 {{style.name}}
       div.style(v-for="variable in style.variables")
@@ -8,9 +9,13 @@
 </template>
 
 <script>
-import { ref, onMounted } from "@nuxtjs/composition-api";
+import { ref, unref, onMounted } from "@nuxtjs/composition-api";
 import ColorPicker from "./ColorPicker.vue";
-import useColors from "~/compositions/useColors.js";
+import useColors from "~/compositions/useColors";
+import {
+  cssVariablesFromString,
+  cssStringToArray
+} from "~/compositions/useColors/helpers.js";
 
 export default {
   components: {
@@ -18,19 +23,68 @@ export default {
   },
   setup(props, context) {
     const {
-      styles,
+      state,
       setStyle,
       removeStyles,
       watchForStyleChanges,
       updateStyles
     } = useColors();
 
+    function copyToClipboard() {
+      let overrideStyles = document.documentElement.style.cssText;
+      if (!overrideStyles) return console.error("No styles");
+
+      const copy = str => {
+        const el = document.createElement("textarea");
+        el.value = str;
+        el.setAttribute("readonly", "");
+        el.style.position = "absolute";
+        el.style.left = "-9999px";
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand("copy");
+        document.body.removeChild(el);
+      };
+
+      // Create a list of all the styles
+      // If an override exists, remove the original
+      const allStyles = unref(state.styles).reduce((res, style) => {
+        for (let kv of style.variables) {
+          // Check if an override for that key exists
+          // Strip to only the variable names
+          const overrideAsArray = cssStringToArray(overrideStyles);
+          const overrideExists = overrideAsArray.find(style => {
+            return style.key === kv.key;
+          });
+
+          if (overrideExists) {
+            // Use the override!
+            console.log("Using override", overrideExists);
+            res.push(`${overrideExists.key}:${overrideExists.value}`);
+          } else {
+            // Use original style
+            res.push(`${kv.key}:${kv.value}`);
+          }
+        }
+
+        return res;
+      }, []);
+
+      context.root.$buefy.toast.open({
+        message: "Copied to clipboard",
+        type: "is-success"
+      });
+
+      console.info("Copied to clipboard", allStyles);
+      copy(allStyles);
+    }
+
     onMounted(() => {
       watchForStyleChanges(); // Watch for changes!
       updateStyles(); // Get the styles
     });
 
-    return { styles, setStyle, removeStyles };
+    return { styles: state.styles, setStyle, copyToClipboard, removeStyles };
   }
 };
 </script>
