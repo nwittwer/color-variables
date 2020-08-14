6675278cbf4bf628772771015b33d2c3a49773c1
Nick Wittwer <nwittwer@clincase.com>
2020-08-14 16:29:56 +0200

    Add copy button for any styles definitions

--- components/sidebar/overrides/ColorVariable.vue
+++ components/sidebar/overrides/ColorVariable.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   div
     button(@click="removeStyles") Remove overrides
-    button(@click="copyToClipboard") Copy overrides
     div(v-for="style of styles")
+      CopyStyleToClipboardButton(:styles="style")
       h4 {{style.name}}
       div.style(v-for="variable in style.variables")
         ColorPicker(:name="variable.key" :value="variable.value" @change="setStyle")
@@ -11,15 +11,13 @@
 <script>
 import { ref, unref, onMounted } from "@nuxtjs/composition-api";
 import ColorPicker from "./ColorPicker.vue";
+import CopyStyleToClipboardButton from "./CopyStyleToClipboardButton.vue";
 import useColors from "~/compositions/useColors";
-import {
-  cssVariablesFromString,
-  cssStringToArray
-} from "~/compositions/useColors/helpers.js";
 
 export default {
   components: {
-    ColorPicker
+    ColorPicker,
+    CopyStyleToClipboardButton
   },
   setup(props, context) {
     const {
@@ -30,61 +28,12 @@ export default {
       updateStyles
     } = useColors();
 
-    function copyToClipboard() {
-      let overrideStyles = document.documentElement.style.cssText;
-      if (!overrideStyles) return console.error("No styles");
-
-      const copy = str => {
-        const el = document.createElement("textarea");
-        el.value = str;
-        el.setAttribute("readonly", "");
-        el.style.position = "absolute";
-        el.style.left = "-9999px";
-        document.body.appendChild(el);
-        el.select();
-        document.execCommand("copy");
-        document.body.removeChild(el);
-      };
-
-      // Create a list of all the styles
-      // If an override exists, remove the original
-      const allStyles = unref(state.styles).reduce((res, style) => {
-        for (let kv of style.variables) {
-          // Check if an override for that key exists
-          // Strip to only the variable names
-          const overrideAsArray = cssStringToArray(overrideStyles);
-          const overrideExists = overrideAsArray.find(style => {
-            return style.key === kv.key;
-          });
-
-          if (overrideExists) {
-            // Use the override!
-            console.log("Using override", overrideExists);
-            res.push(`${overrideExists.key}:${overrideExists.value}`);
-          } else {
-            // Use original style
-            res.push(`${kv.key}:${kv.value}`);
-          }
-        }
-
-        return res;
-      }, []);
-
-      context.root.$buefy.toast.open({
-        message: "Copied to clipboard",
-        type: "is-success"
-      });
-
-      console.info("Copied to clipboard", allStyles);
-      copy(allStyles);
-    }
-
     onMounted(() => {
       watchForStyleChanges(); // Watch for changes!
       updateStyles(); // Get the styles
     });
 
-    return { styles: state.styles, setStyle, copyToClipboard, removeStyles };
+    return { styles: state.styles, setStyle, removeStyles };
   }
 };
 </script>
