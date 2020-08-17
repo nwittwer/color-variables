42f748c793dd053546e563d6b5778a750f1d55b8
Nick Wittwer <nwittwer@clincase.com>
2020-08-17 16:16:16 +0200

    Added :active button state color

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,8 @@
       button.button--secondary Button
     section
       .dropdown
-        .dropdown__label Actions...
+        .dropdown__trigger
+          button.button--primary Actions...
         .dropdown__content
           p Hi there
     section
@@ -34,6 +35,8 @@
           h2 Title
           .divider
           p Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.
+          button.button--primary Button
+          button.button--secondary Button
         .modal__background
     section
       .card
@@ -187,6 +190,10 @@ button {
     background: var(--brand-primary-color);
 
     &:hover {
+      background: var(--brand-primary-color-light);
+    }
+
+    &:active {
       background: var(--brand-primary-color-dark);
     }
   }
@@ -196,6 +203,10 @@ button {
     background: var(--brand-secondary-color);
 
     &:hover {
+      background: var(--brand-secondary-color-light);
+    }
+
+    &:active {
       background: var(--brand-secondary-color-dark);
     }
   }
